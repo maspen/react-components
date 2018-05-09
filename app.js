954c0a46531e6a8cb16281ac79ca08d51e4a3021
Matt Aspen <maspen@Matts-MacBook-Pro.local>
2018-05-09 10:40:07 -0700

    got list of 2 grocery items to render on page

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-// TODO
+class GroceryList extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			groceries: [
+				'bananas',
+				'beer'
+			]
+		};
+	}
+
+	render() {
+		return (
+			<div id="grocery-list">
+				<div>{this.state.groceries[0]}</div>
+				<div>{this.state.groceries[1]}</div>
+			</div>
+		);
+	}
+}
+// <div id="app"></div>
+ReactDOM.render(
+  <GroceryList />,
+  document.getElementById('app')
+);
